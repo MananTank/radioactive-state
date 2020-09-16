b93882bbbbe2ee819ed67e0010bf770bfe3c8330
Manan Tank <manantankm@gmail.com>
2020-09-16 15:47:06 +0530

    update comment, code refactor

--- utils/getOnChange.js
+++ utils/getOnChange.js
@@ -2,15 +2,17 @@ import getRS from './getRS'
 import {silentMutate} from './mutate'
 import afterSync from './afterSync'
 
-// forceUpdate is called not for all onChange, but after all onChanges are called
-// this is because onChange may be called multiple times for a single mutation
-// calling forceUpdate after all the sync code has executed, essentially batches all of them into one
+/* getOnChange returns an onChange function
+ * this onChange function batches all the mutations that take place in RS.current
+ * and then calls forceUpdate to trigger a re-render
+ */
 
 const getOnChange = (RS, forceUpdate) => {
   const timer = { set: false }
+
   const onChange = (chain, value, trap, now) => {
-    // if adding an object, make it radioactive
-    const rValue = (typeof value === 'object' && trap === 'set') ? getRS(value, onChange, chain) : value
+    const addingObject = typeof value === 'object' && trap === 'set'
+    const rValue =  addingObject ? getRS(value, onChange, chain) : value
     const success = silentMutate(RS.current, chain, rValue, trap)
     if (now) forceUpdate()
     else if (!timer.set) afterSync(forceUpdate, timer)
