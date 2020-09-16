7635288d400547bd3f7fec8abc53a681f1262607
Manan Tank <manantankm@gmail.com>
2020-09-16 15:29:54 +0530

    update comments in afterSync.js

--- utils/afterSync.js
+++ utils/afterSync.js
@@ -1,8 +1,5 @@
-// run the given function after all the sync code is completed
-// timer is a variable which keeps track of whether the setTimeout is set or not
-// this is useful if you want the fn to be called only once,
-// for that you can check the value of timer.set
-// only call afterSync if timer.set is false
+// call fn() after all the sync code is completed
+// call afterSync() if timer.set === false to ensure that fn is only called once
 
 const afterSync = (fn, timer) => {
   timer.set = true
