8ef248299bd0fa8c0fc78fc54aaa1d138bd543af
Manan Tank <manantankm@gmail.com>
2020-09-16 15:41:12 +0530

    fix no error on null initial state

--- utils/errors.js
+++ utils/errors.js
@@ -1,9 +1,9 @@
 export const checkInitialState = initialState => {
-  const msg = `Invalid Initial State: Expected a reference type, but got "${initialState}" instead.\n` +
+  const msg = `Invalid initial state: Expected a reference type, Got "${initialState}" instead.\n` +
   'useRS() hook takes a reference type as an argument, such as object/array ' +
   'OR a function that returns an object/array.'
 
-  if (typeof initialState !== 'object' && initialState !== null) {
+  if (typeof initialState !== 'object' || initialState === null) {
     if (process.env.NODE_ENV !== 'production') throw new Error(msg)
     else console.error(msg)
   }
