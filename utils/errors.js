e4d724f48737c5e34c1c961e4671fd531b3f8d47
Manan Tank <manantankm@gmail.com>
2021-02-06 17:03:12 +0530

    shorten error message

--- utils/errors.js
+++ utils/errors.js
@@ -1,14 +1,7 @@
 const isObject = require('./isObject')
 
 const checkInitialState = initialState => {
-  const msg = `Invalid initial state: Expected a reference type, Got "${initialState}" instead.\n` +
-  'useRS() hook takes a reference type as an argument, such as object/array ' +
-  'OR a function that returns an object/array.'
-
-  if (!isObject(initialState)) {
-    if (process.env.NODE_ENV !== 'production') throw new Error(msg)
-    else console.error(msg)
-  }
+  if (!isObject(initialState)) throw new Error(`Invalid State "${initialState}" provided to useRS() hook, it must be either an object/array or a function that returns an object/array`)
 }
 
 module.exports = {
